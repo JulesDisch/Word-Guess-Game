02ac0f87ff57b477829101cc2afe122c790881d5
JulesDisch <53347157+JulesDisch@users.noreply.github.com>
2019-10-15 23:50:31 -0400

    I have made many updates:     new images for each band option     new logic using jQuery     streamlined css     updated the README     created modals instead of alerts

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Word-Guess-Game
 
-This is my word guess game, and this is the read me file.
+This project is a word guess/hangman type game, using JavaScript for the logic and jQuery to manipulate HTML, as well as a custom CSS.
+All of the possible words are 90s bands, and the theme is based off of a popular site for finding lyrics, www.AZLyrics.com.
+Even though AZLyrics got its start in 2000, I have always loved how they have seemingly never updated the site's appearance in the last 20 years.
+The game uses keyup functionality to advance, as well as modals. The modals show the correct answer, along with the album art of one of that band's 90s albums.
+The modals can be closed by clicking the "keep playing" button, hitting the "enter" or "space" keys, or clicking the mouse outside the modal.
+The win/loss record is kept track of at the bottom of the screen, and once the user has gone through all 9 bands that are options for the game, they will see "game over" and their final score.
+They will then have the option to play again, which will randomize the order of the bands, and put their win/loss record back at 0/0.
+The deployed version can be found at https://julesdisch.github.io/Word-Guess-Game/index.html
 
-I chose 90s bands that are only one word because I couldn't figure out if adding the option for spaces in addition to letters was possible with the code.
-
-while the theme looks kinda lame, it's in the spirit of www.azlyrics.com: a site that has never changed its appearance since it first launched. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,39 @@
-var bands = ["nirvana", "soundgarden", "garbage", "sublime", "oasis", "everclear", "weezer", "tool", "bush"];
+var bands = ["nirvana", "soundgarden", "garbage", "sublime", "oasis", "everclear", "weezer", "tool", "bush",];
 var band;
+var bandImg;
 var GuessedArray;
 var blanks;
 var wins = 0;
 var losses = 0;
 var guesses = 0;
+var j = 0;
 var finishedWord = false;
 
-// what we need for page load
+randomize();
+
+function randomize() {
+    bands.sort(function (a, b) { return 0.5 - Math.random() })
+}
+
 function newGame() {
-    band = bands[Math.floor(Math.random() * bands.length)];
+    $("#start").hide();
+    $("#over").hide();
+    $("#over1").hide();
+    this.band = bands[j];
+    console.log(j)
+    bandImg = $("<img src='assets/images/" + band + ".jpg" + "' alt='" + band + "'>");
     blanks = [];
     for (var i = 0; i < band.length; i++) { blanks.push("_ "); }
     GuessedArray = [];
     updateGuessCount();
     updateDisplay();
 };
+
 function updateGuessCount() {
     guesses = band.length + 5;
     guessesRemaining = guesses;
 };
+
 // where updates take place
 function updateDisplay() {
     var blanksText = " ";
@@ -49,34 +63,151 @@ function evaluateLetter(letter) {
         }
     }
 };
+
+function winModal() {
+    if (j === 8) { gameOverModal() }
+    else {
+        setTimeout(function () {
+            var winModal = document.getElementById("youWin");
+            $("#band-image").append(bandImg);
+            winModal.style.display = "block";
+            j++
+            wins++;
+            newGame();
+            var contBtn = document.getElementById("continue");
+            contBtn.onclick = function () {
+                winModal.style.display = "none";
+                $("#band-image").empty();
+                finishedWord = true;
+            }
+
+            window.onclick = function (event) {
+                if (event.target === winModal) {
+                    winModal.style.display = "none";
+                    $("#band-image").empty();
+                    finishedWord = true;
+                }
+            }
+
+            window.onkeyup = function (event) {
+                if ((event.keyCode === 13) || (event.keyCode === 32)) {
+                    winModal.style.display = "none";
+                    $("#band-image").empty();
+                    finishedWord = true;
+                }
+            }
+        }, 300);
+    }
+}
+
+function loseModal() {
+    if (j === 8) { gameOverModal() }
+    else {
+        setTimeout(function () {
+            var loseModal = document.getElementById("youLose");
+            $("#band-image2").append(bandImg);
+            var contBtn = document.getElementById("cont");
+            loseModal.style.display = "block";
+            $("#answer").html("The answer was " + band);
+            j++
+            losses++;
+            newGame();
+            contBtn.onclick = function () {
+                loseModal.style.display = "none";
+                $("#band-image2").empty();
+                finishedWord = true;
+            }
+
+            window.onclick = function (event) {
+                if (event.target === loseModal) {
+                    loseModal.style.display = "none";
+                    $("#band-image2").empty();
+                    finishedWord = true;
+                }
+            }
+
+            window.onkeyup = function (event) {
+                if ((event.keyCode === 13) || (event.keyCode === 32)) {
+                    loseModal.style.display = "none";
+                    $("#band-image2").empty();
+                    finishedWord = true;
+                }
+            }
+        }, 300);
+    }
+}
+
+function gameOverModal() {
+    setTimeout(function () {
+        if (blanks.indexOf("_ ") === -1) {
+            $("#winOrLose").text("You win!");
+        }
+        else if (guesses <= 0) {
+            $("#winOrLose").text("You lose!");
+            $("#answer2").html("The answer was " + band);
+        }
+        var overModal = document.getElementById("game-over");
+        $("#band-image3").append(bandImg);
+        var overBtn = document.getElementById("ok");
+        var span = document.getElementsByClassName("close")[0];
+        overModal.style.display = "block";
+        overBtn.onclick = function () {
+            overModal.style.display = "none";
+            $("#currentWord-text").hide();
+            $("#blanks-text").hide();
+            $("#start").show();
+            $("#GuessedArray-text").text("Final Score:");
+            $("#start").on("click", run);
+        }
+
+        span.onclick = function () {
+            overModal.style.display = "none";
+            $("#currentWord-text").hide();
+            $("#blanks-text").hide();
+            $("#start").show();
+            $("#GuessedArray-text").text("Final Score:");
+            $("#start").on("click", run);
+        }
+
+        window.onclick = function (event) {
+            if (event.target == overModal) {
+                overModal.style.display = "none";
+                $("#currentWord-text").hide();
+                $("#blanks-text").hide();
+                $("#start").show();
+                $("#GuessedArray-text").text("Final Score:");
+                $("#start").on("click", run);
+            }
+        }
+    }, 300);
+}
+
+
+function run() {
+    j = 0;
+    wins = 0;
+    losses = 0;
+    guesses = 0;
+    finishedWord = false;
+    $("#currentWord-text").show();
+    $("#blanks-text").show();
+    $("#start").hide();
+    randomize();
+    newGame();
+}
+
 function checkWin() {
     if (blanks.indexOf("_ ") === -1) {
-        setTimeout(function () {
-            alert("You win!");
-            wins++;
-            finishedWord = true;
-            playAgain();
-        }, 500);
+        winModal();
     }
+
 };
 function checkLoss() {
     if (guesses <= 0) {
-        setTimeout(function () {
-            alert("You lose!");
-            losses++;
-            finishedWord = true;
-            playAgain();
-        }, 500);
+        loseModal();
     }
 };
-// asks player if they want a new word
-function playAgain() {
-    var restart = confirm("Would you like to play again?");
-    if (restart == true) {
-        newGame();
-    } else {
-    }
-}
+
 // creates the guessed letters section
 function guessLetter(letter) {
     if (guessesRemaining > 0) {
@@ -91,7 +222,7 @@ function guessLetter(letter) {
 newGame();
 document.onkeyup = function (event) {
     if (event.keyCode >= 49 && event.keyCode <= 90) {
-        console.log(blanks);
+        $("#Directions-text").hide();
         evaluateLetter(event.key.toLowerCase());
         updateDisplay();
         checkWin();
